b135174d3c65b42b35c36ce7863812924a790d33
xmdhs <xmdhss@gmail.com>
2025-05-16 12:00:10 +0800

    隐藏

--- main.go
+++ main.go
@@ -3,15 +3,33 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"log"
 	"strconv"
+	"strings"
 	"sync/atomic"
+	"syscall"
 	"time"
+	"unsafe"
 
 	"context"
 
 	"github.com/getlantern/systray"
 	"github.com/go-vgo/robotgo"
 	"github.com/ncruces/zenity"
+	"golang.org/x/sys/windows"
+)
+
+var (
+	user32                   = windows.NewLazySystemDLL("user32.dll")
+	procEnumWindows          = user32.NewProc("EnumWindows")
+	procGetWindowTextW       = user32.NewProc("GetWindowTextW")
+	procGetWindowTextLengthW = user32.NewProc("GetWindowTextLengthW")
+	procIsWindowVisible      = user32.NewProc("IsWindowVisible")
+	procShowWindow           = user32.NewProc("ShowWindow")
+)
+
+const (
+	SW_MINIMIZE = 6
 )
 
 //go:embed logo.ico
@@ -33,6 +51,7 @@ func main() {
 		mCancel:  mCancel,
 	}
 	go m.do(mCtx)
+	go run(ctx)
 	systray.Run(m.onReady, m.onExit)
 }
 
@@ -109,6 +128,81 @@ func (m *mouse) do(ctx context.Context) {
 				robotgo.MoveRelative(-move, -move)
 				has = true
 			}
+		}
+	}
+}
+
+// 枚举窗口的回调函数
+type enumWindowsProc func(hwnd syscall.Handle, lParam uintptr) bool
+
+func enumWindows(callback enumWindowsProc, lParam uintptr) error {
+	cb := syscall.NewCallback(func(hwnd syscall.Handle, lParam uintptr) uintptr {
+		if callback(hwnd, lParam) {
+			return 1 // continue
+		}
+		return 0 // stop
+	})
+	ret, _, err := procEnumWindows.Call(cb, lParam)
+	if ret == 0 {
+		return err
+	}
+	return nil
+}
+
+// 获取窗口标题
+func getWindowText(hwnd syscall.Handle) string {
+	length, _, _ := procGetWindowTextLengthW.Call(uintptr(hwnd))
+	if length == 0 {
+		return ""
+	}
+	buf := make([]uint16, length+1)
+	procGetWindowTextW.Call(uintptr(hwnd), uintptr(unsafe.Pointer(&buf[0])), length+1)
+	return syscall.UTF16ToString(buf)
+}
+
+// 判断窗口是否可见
+func isWindowVisible(hwnd syscall.Handle) bool {
+	ret, _, _ := procIsWindowVisible.Call(uintptr(hwnd))
+	return ret != 0
+}
+
+// 最小化窗口
+func minimizeWindow(hwnd syscall.Handle) {
+	procShowWindow.Call(uintptr(hwnd), SW_MINIMIZE)
+}
+
+func run(ctx context.Context) {
+	c := time.Tick(1 * time.Minute)
+	for {
+		hidesList := []syscall.Handle{}
+		needHide := true
+
+		err := enumWindows(func(hwnd syscall.Handle, lParam uintptr) bool {
+			if !isWindowVisible(hwnd) {
+				return true
+			}
+			title := getWindowText(hwnd)
+			if strings.Contains(title, "企业微信") {
+				hidesList = append(hidesList, hwnd)
+			}
+			if strings.Contains(title, "RustDesk") {
+				needHide = false
+			}
+			return true
+		}, 0)
+		if err != nil {
+			log.Println("发生错误：", err)
+		}
+		if needHide {
+			for _, v := range hidesList {
+				minimizeWindow(v)
+			}
+		}
+
+		select {
+		case <-c:
+		case <-ctx.Done():
+			return
 		}
 	}
 }
